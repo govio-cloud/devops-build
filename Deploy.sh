898328550209b9b7c0ea8d1036b41d924e3b50bb
Govindaraj <govind@devops.com>
2025-02-27 16:00:16 +0000

    deploy script changed

--- Deploy.sh
+++ Deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e
 
-docker-compose down
+# Define Docker container name and image
+CONTAINER_NAME="my-container"
+IMAGE_NAME="govio-cloud/devops-build:latest"  # You can update this dynamically based on branch or other criteria
 
-docker-compose up -d
+# Stop and remove any running container with the same name
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+# Run the new container from the latest image
+docker run -d --name $CONTAINER_NAME -p 8081:80 $IMAGE_NAME
+
+echo "Deployment complete!"
+
